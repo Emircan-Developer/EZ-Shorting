188cecfad30c84ff0326f6453a8e1f828b61d2cc
Emircan-Developer <emircan_bostanci@hotmail.com>
2020-08-24 23:34:39 +0300

    v1

---
+++ EzSorter/Bubble.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EzSorter
+{
+    public class Bubble : ISorter
+    {
+        public int[] Sort(int[] Array)
+        {
+            int temp;
+            for (int j = 0; j < Array.Length; j++)
+            {
+                for (int i = 1; i < Array.Length; i++)
+                {
+                    temp = Array[i - 1];
+                    if (Array[i] < Array[i - 1])
+                    {
+                        Array[i - 1] = Array[i];
+                        Array[i] = temp;
+
+                    }
+                }
+            }
+            return Array;
+        }
+
+        public float[] Sort(float[] Array)
+        {
+            float temp;
+            for (int j = 0; j < Array.Length; j++)
+            {
+
+                for (int i = 1; i < Array.Length; i++)
+                {
+                    temp = Array[i - 1];
+                    if (Array[i] > Array[i - 1])
+                    {
+                        Array[i - 1] = Array[i];
+                        Array[i] = temp;
+
+                    }
+                }
+            }
+
+            return Array;
+        }
+
+        public double[] Sort(double[] Array)
+        {
+            double temp;
+            for (int j = 0; j < Array.Length; j++)
+            {
+
+                for (int i = 1; i < Array.Length; i++)
+                {
+                    temp = Array[i - 1];
+                    if (Array[i] < Array[i - 1])
+                    {
+                        Array[i - 1] = Array[i];
+                        Array[i] = temp;
+
+                    }
+                }
+            }
+
+            return Array;
+        }
+
+        /*  public string Sort(string[] Array)
+          {
+              for(int i = 0;i < Array.Length; i++)
+              {
+                  string
+              }
+          }*/
+    }
+}
---
+++ EzSorter/ISorter.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EzSorter
+{
+    public interface ISorter
+    {
+        int[] Sort(int[] Array);
+        float[] Sort(float[] Array);
+        double[] Sort(double[] Array);
+    //    string Sort(string[] Array);
+    }
+}
